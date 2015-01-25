a0c220b35ae3d4ba2f273bec212b537836f854f0
Syohei YOSHIDA <syohex@gmail.com>
2015-01-25 16:14:58 +0900

    Bump up version and update Changes

--- terraform-mode.el
+++ terraform-mode.el
@@ -1,10 +1,10 @@
 ;;; terraform-mode.el --- Major mode for terraform configuration file
 
-;; Copyright (C) 2014 by Syohei YOSHIDA
+;; Copyright (C) 2015 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-terraform-mode
-;; Version: 0.01
+;; Version: 0.02
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
