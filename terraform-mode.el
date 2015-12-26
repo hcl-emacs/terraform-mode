6157690efce03ff20b16b85e2be6b0964efec2fb
Syohei YOSHIDA <syohex@gmail.com>
2015-12-26 21:57:06 +0900

    Bump up version and update Changes

--- terraform-mode.el
+++ terraform-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-terraform-mode
-;; Version: 0.03
+;; Version: 0.04
 ;; Package-Requires: ((hcl-mode "0.01") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
